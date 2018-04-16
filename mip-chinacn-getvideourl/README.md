b16c7547a56fe30e82e5d261fa022bd431559721
wangsha <wangsha@mail.china.cn>
2018-04-16 16:25:56 +0800

    添加视频播放组件

---
+++ mip-chinacn-getvideourl/README.md
@@ -0,0 +1,20 @@
+# mip-chinacn-getvideourl
+
+mip-chinacn-getvideourl
+
+标题|内容
+----|----
+类型|业务
+支持布局|responsive, fixed-height, fill, container, fixed
+所需脚本|https://c.mipcdn.com/static/v1/mip-chinacn-getvideourl/mip-chinacn-getvideourl.js
+
+## 示例
+
+### 基本使用
+
+```html
+<mip-chinacn-getvideourl></mip-chinacn-getvideourl>
+```
+## 属性
+
+## 注意事项
---
+++ mip-chinacn-getvideourl/mip-chinacn-getvideourl.js
@@ -0,0 +1,43 @@
+/**
+ * @author wangsha@mail.china.cn
+ *
+ * @file mip-chinacn-getvideourl 组件
+ */
+define(function (require) {
+    var customElement = require('customElement').create();
+    var md5 = require('./md5.min');
+    var fetchJsonp = require('fetch-jsonp');
+
+    /**
+     * 构造元素，只会运行一次
+     */
+    customElement.prototype.firstInviewCallback = function () {
+        var self = this;
+        var element = self.element;
+        var myVideo = element.querySelector('video');
+        var request = false;
+        myVideo.addEventListener('play', function () {
+            if (request === true) {
+                return;
+            } else {
+                myVideo.pause();
+                var id = element.querySelector('#id').value;
+                var k = md5('chinavideoplay');
+                fetch('https://www.china.cn/video_api.php?a=play&k=' + k + '&t=mip&id=' + id + '')
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (myJson) {
+                        request = true;
+                        var videoUrl = myJson.data;
+                        myVideo.setAttribute('src', videoUrl);
+                        myVideo.play();
+                    }).catch(function (e) {
+                        console.log(e);
+                    });
+            }
+        });
+    };
+    return customElement;
+
+});
